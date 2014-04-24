c7f3a69a68a79f4ed8fff41ef3d7af4432fe25a2
Axel Smeets <murlocbrand+git@gmail.com>
2014-04-24 23:41:07 +0200

    Added placeholder rss-reader. - Cannot read images because they aren't strictly inside the <item> tag. - Uses a hack for cross-domain ajax :3

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
-function woppa(){
-    alert("yoyo js include here!");
+var x2js = new X2JS();
+
+function parseXML(xmlText) {
+    var jsonObj = x2js.xml_str2json( xmlText );
+    return jsonObj; 
 }
+
+function loadRSS(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://script.google.com/a/macros/mcpher.com/s/AKfycbzGgpLEWS0rKSBqXG5PcvJ7Fpe02fvGqiCqq54SVQmBJSpy_6s/exec?url=" + encodeURIComponent(url), true);
+    xhr.responseType = 'json';
+
+    xhr.onload = function() {
+        //var parser = new DOMParser();
+        //var doc = parser.parseFromString(xhr.response.results, "text/xml");
+
+        callback(xhr.response.results);
+    };
+
+    xhr.onerror = function() {
+        alert('Woops, there was an error making the request.');
+    };
+
+    xhr.send();
+
+}
+
+function update(){
+    loadRSS("http://www.felicious.se/RSS/blog", function(xmlDoc){
+        var feed = parseXML(xmlDoc);
+        console.log(feed);
+    });
+    //console.log(rss);
+}
+
+document.onload = update();
