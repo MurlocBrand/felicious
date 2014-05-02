03e8dab316e92c521d00b1281c72241e9bf35bb6
Axel Smeets <murlocbrand+git@gmail.com>
2014-05-02 16:12:30 +0200

    Reworked layout and css - Removed grid.css - Removed header.png - Added freebooter font - Fixed webview for Adfero bottom margin error

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var x2js = new X2JS();
 
+var fullText = [];
+
 function parseXML(xmlText) {
     var jsonObj = x2js.xml_str2json( xmlText );
     return jsonObj; 
@@ -7,67 +9,85 @@ function parseXML(xmlText) {
 
 function loadRSS(url, callback) {
     var jqxhr = $.get( "https://script.google.com/a/macros/mcpher.com/s/AKfycbzGgpLEWS0rKSBqXG5PcvJ7Fpe02fvGqiCqq54SVQmBJSpy_6s/exec?url=" + encodeURIComponent(url), function(data) {
-        alert( "success" );
         callback(data.results);
     })
     .fail(function() {
-        alert( "error" );
+        alert( "error - couldn't get blog data" );
     })
+}
+
+function showText(id){
+    $("#text-" + id).html(fullText[id]);
+    $("#overlay-" + id).hide();
 }
 
 function update(){
     console.log("running update");
     loadRSS("http://www.felicious.se/RSS/blog", function(xmlDoc){
         var feed = parseXML(xmlDoc);
-        console.log(feed);
         var blog = feed.rss.channel.item;
-        // le hax, we insert all the posts before the sentinel
-        var sentinel = document.getElementById("ggsentinel");
-        var container = document.getElementById("container");
+
+        var container = $("#container");
+        var postId = 0;
 
         blog.forEach(function(entry) {
-            // create the class attributes
-            var cardClass = document.createAttribute("class");
-            cardClass.nodeValue = "mobile-prefix-5 mobile-grid-90 mobile-suffix-5 prefix-5 grid-90 card";
-            var contentClass = document.createAttribute("class");
-            contentClass.nodeValue = "mobile-prefix-5 mobile-grid-90 prefix-5 grid-90 card-content";
-            var imgClass = document.createAttribute("class");
-            imgClass.nodeValue = "mobile-prefix-5 mobile-grid-90 prefix-5 grid-90 card-img visible-img";
+            var card = $("<div>").addClass("card");
+            var title = $("<h1>");
+            var content = $("<p>");
+            var body = $("<div>").addClass("body1");
 
+            title.html(entry.title.toString());
 
-            var card = document.createElement("div");
-            var content = document.createElement("div");
-            var title = document.createElement("h1");
-            var images = document.createElement("div");
-
-            title.appendChild(document.createTextNode(entry.title.toString()));
             // attempt to split on the image tag
             var data = entry.encoded.__cdata.split("<img");
             var imgSrc, image, srcAtt;
+
             // if we have an image
             if (data[1]) {
                 // extract the src attribute
                 imgSrc = data[1].split(" ")[1];
+
                 // remove the preceding src=", and the last "
                 imgSrc = imgSrc.substr(5, imgSrc.length-6)
 
-                srcAtt = document.createAttribute("src");
-                srcAtt.nodeValue = imgSrc;
-                image = document.createElement("img");
-                image.setAttributeNode(imgClass);
-                image.setAttributeNode(srcAtt);
+                image = $("<div>").addClass("card-image");
+                image.css("background-image", "url('" + imgSrc + "')");
             }
-            // set the text of the blogpost
-            content.innerHTML = data[0];
-            content.setAttributeNode(contentClass);
 
-            card.appendChild(title)
+            // set the text of the blogpost
+            content.html(data[0]);
+            content.attr("id", "text-" + postId);
+
+            card.append(title);
             if (image) {
-                card.appendChild(image);
+                card.append(image);
             }
-            card.appendChild(content);
-            card.setAttributeNode(cardClass);
-            container.insertBefore(card, sentinel);
+
+            body.append(content);
+            card.append(body);
+            if (data[0].length > 100) {
+                var overlay = $("<div>");
+                overlay.attr("id", "overlay-" + postId);
+                overlay.addClass("image-overlay");
+
+                var link = $("<a>");
+                link.attr("href", "javascript:showText('" + postId +"');");
+
+                var imgOverlay = $("<img>");
+                imgOverlay.attr("src", "trans.gif");
+                imgOverlay.attr("width", "100%").attr("height", "100%");
+
+                link.append(imgOverlay);
+                overlay.append(link);
+
+                fullText[postId] = data[0];
+
+                content.html(data[0].substr(0, 100));
+                body.append(overlay);
+            }
+            container.append(card);
+
+            postId++;
         });
     });
 }
