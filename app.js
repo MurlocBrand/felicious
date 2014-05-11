d89171c0ba2216a6ea96f756c8afaf68775f2220
Axel Smeets <murlocbrand+git@gmail.com>
2014-05-11 21:26:07 +0200

    Added caching feature. App will now use local storage (totalStorage.js) to cache blog posts.
    When the app starts, all posts in the cache are rendered immediately to
    the DOM. While rendering, the app will also try to retrieve the RSS feed
    from www.felicious.com, and render only the new items.
    
    Cached posts are rendered under the 'cached-items-node' div and new
    items are rendered under the 'new-items-node' div. Check index.html
    
    This feature makes the blog load almost instantly.
    Look into adding a [+10]-like button if user is scrolling while update
    finishes, so that the scrolling isn't messed up.

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var x2js = new X2JS();
-
 var fullText = [];
+var postId = 0;
 
 function parseXML(xmlText) {
-    var jsonObj = x2js.xml_str2json( xmlText );
-    return jsonObj; 
+    return x2js.xml_str2json( xmlText ); 
 }
 
 function loadRSS(url, callback) {
@@ -12,7 +11,7 @@ function loadRSS(url, callback) {
         callback(data.results);
     })
     .fail(function() {
-        alert( "error - couldn't get blog data" );
+        alert( "error - couldn't get blog data. Make sure you have a working internet connection and restart the app." );
     })
 }
 
@@ -21,77 +20,90 @@ function showText(id){
     $("#overlay-" + id).hide();
 }
 
+function renderBlogItems (container, blog) {
+    blog.forEach(function(entry) {
+        var card = $("<div>").addClass("card");
+        var title = $("<h1>");
+        var content = $("<p>");
+        var body = $("<div>").addClass("body1");
+
+        title.html(entry.title.__cdata);
+
+        // attempt to split on the image tag
+        var data = entry.encoded.__cdata.split("<img");
+        var imgSrc, image, srcAtt;
+
+        // if we have an image
+        if (data[1]) {
+            // extract the src attribute
+            imgSrc = data[1].split(" ")[1];
+
+            // remove the preceding src=", and the last "
+            imgSrc = imgSrc.substr(5, imgSrc.length-6)
+
+            image = $("<div>").addClass("card-image");
+            image.css("background-image", "url('" + imgSrc + "')");
+        }
+
+        // set the text of the blogpost
+        content.html(data[0]);
+        content.attr("id", "text-" + postId);
+
+        card.append(title);
+        if (image) {
+            card.append(image);
+        }
+
+        body.append(content);
+        card.append(body);
+        if (data[0].length > 4 * 35) {
+            var overlay = $("<div>");
+            overlay.attr("id", "overlay-" + postId);
+            overlay.addClass("image-overlay");
+
+            var link = $("<a>");
+            link.attr("href", "javascript:showText('" + postId +"');");
+
+            var imgOverlay = $("<img>");
+            imgOverlay.attr("src", "trans.gif");
+            imgOverlay.attr("width", "100%").attr("height", "100%");
+
+            link.append(imgOverlay);
+            overlay.append(link);
+
+            fullText[postId] = data[0];
+
+            content.html(data[0].substr(0, data[0].indexOf(" ", 120)));
+            body.append(overlay);
+        }
+        container.append(card);
+
+        postId++;
+    });
+}
+
 function update(){
-    console.log("running update");
     loadRSS("http://www.felicious.se/RSS/blog", function(xmlDoc){
         var feed = parseXML(xmlDoc);
         var blog = feed.rss.channel.item;
+        var currentItems = $.totalStorage('cached-blog');
 
-        var container = $("#container");
-        var postId = 0;
-
-        blog.forEach(function(entry) {
-            var card = $("<div>").addClass("card");
-            var title = $("<h1>");
-            var content = $("<p>");
-            var body = $("<div>").addClass("body1");
-
-            title.html(entry.title.toString());
-
-            // attempt to split on the image tag
-            var data = entry.encoded.__cdata.split("<img");
-            var imgSrc, image, srcAtt;
-
-            // if we have an image
-            if (data[1]) {
-                // extract the src attribute
-                imgSrc = data[1].split(" ")[1];
-
-                // remove the preceding src=", and the last "
-                imgSrc = imgSrc.substr(5, imgSrc.length-6)
-
-                image = $("<div>").addClass("card-image");
-                image.css("background-image", "url('" + imgSrc + "')");
+        $.totalStorage('cached-blog', blog);
+        
+        if (currentItems) {
+            for (var i = 0; i < currentItems.length; i++) {
+                blog.pop();    
             }
-
-            // set the text of the blogpost
-            content.html(data[0]);
-            content.attr("id", "text-" + postId);
-
-            card.append(title);
-            if (image) {
-                card.append(image);
-            }
-
-            body.append(content);
-            card.append(body);
-            if (data[0].length > 100) {
-                var overlay = $("<div>");
-                overlay.attr("id", "overlay-" + postId);
-                overlay.addClass("image-overlay");
-
-                var link = $("<a>");
-                link.attr("href", "javascript:showText('" + postId +"');");
-
-                var imgOverlay = $("<img>");
-                imgOverlay.attr("src", "trans.gif");
-                imgOverlay.attr("width", "100%").attr("height", "100%");
-
-                link.append(imgOverlay);
-                overlay.append(link);
-
-                fullText[postId] = data[0];
-
-                content.html(data[0].substr(0, 100));
-                body.append(overlay);
-            }
-            container.append(card);
-
-            postId++;
-        });
+        }
+        
+        renderBlogItems($("#new-items-node"), blog);
     });
 }
 
 $(document).ready(function(){
+
+    if ($.totalStorage('cached-blog'))
+        renderBlogItems($("#cached-items-node"), $.totalStorage('cached-blog'));
+    
     update();
 });
